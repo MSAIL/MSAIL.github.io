9c58a49dbf1ec910249dc0f91b5b09eea0eef03a
samtenka <samtenka@umich.edu>
2017-09-29 23:49:23 -0400

    test links

--- gen_table.py
+++ gen_table.py
@@ -6,10 +6,21 @@
 
 import sys
 
-def rows_from_csv(csvnm, delimiter=';'):
+def rows_from_csv(csvnm, delimiter=';', line_delimiter='@'):
     with open(csvnm) as f:
-        lines = f.read().split('\n') 
+        lines = f.read().split('@') 
     return [[w.strip() for w in l.split(delimiter)] for l in lines if l.strip()]
+
+def linkify(element):
+    lines = filter(None, element.split('\n'))
+    def link(line):  
+        if line.startswith('$'):
+            tokens = filter(None, line[1:].split(' '))
+            text = ' '.join(tokens[:-1])
+            url = tokens[-1] 
+            return '<a href="%s"> %s </a>' % (url, text)
+        return line 
+    return ' '.join(link(l) for l in lines)
      
 def table_from_rows(rows, outnm):
     head = '<table align="center">\n'
@@ -18,7 +29,7 @@ def table_from_rows(rows, outnm):
     for row in rows:
         body += '%s<tr>\n' % (4*' ')
         for el in row:
-            body += '%s<td> %s </td>\n' % (8*' ', el)
+            body += '%s<td> %s </td>\n' % (8*' ', linkify(el))
         body += '%s</tr>\n' % (4*' ')
     with open(outnm, 'w') as f:
         f.write(head+body+foot) 
